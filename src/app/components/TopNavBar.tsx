95215e58af0c760ff69992d113a7269faaa37b05
Matthew Incardona <doublethink.inc@gmail.com>
2025-12-26 23:09:50 -0500

    feat: change navbar style

--- src/app/components/TopNavBar.tsx
+++ src/app/components/TopNavBar.tsx
@@ -1,37 +1,26 @@
 "use client";
-import { usePathname } from "next/navigation";
-import Link from "next/link";
-import Image from "next/image";
-import logoSrc from "public/logo.svg";
-import { cx } from "lib/cx";
 
 export const TopNavBar = () => {
-  const pathName = usePathname();
-  const isHomePage = pathName === "/";
-
   return (
-    <header
-      aria-label="Site Header"
-      className={cx(
-        "flex h-[var(--top-nav-bar-height)] items-center border-b-2 border-gray-100 px-3 lg:px-12",
-        isHomePage && "bg-dot"
-      )}
-    >
-      <div className="flex h-10 w-full items-center justify-between">
-        <Link href="/">
-          <span className="sr-only">OpenResume</span>
-          {/* <Image
-            src={logoSrc}
-            alt="Stations Logo"
-            className="h-8 w-full"
-            priority
-          /> */}
-        </Link>
-        <nav
-          aria-label="Site Nav Bar"
-          className="flex items-center gap-2 text-sm font-medium"
-        ></nav>
+    <nav className="m-3 flex max-w-full justify-center text-white">
+      <div className="max-w-400 flex h-12 grow items-center justify-between rounded-xl bg-[#4C73F2] p-8">
+        <div className="flex gap-4">
+          <a href="/">
+            <p>
+              <b>Stations</b>
+            </p>
+          </a>
+          <p>Entry Level Jobs For Tech People</p>
+        </div>
+        <ul className="flex gap-4">
+          <li className="underline">
+            <a href="">Find Jobs</a>
+          </li>
+          <li className="underline">
+            <a href="">Test Your Resume</a>
+          </li>
+        </ul>
       </div>
-    </header>
+    </nav>
   );
 };
