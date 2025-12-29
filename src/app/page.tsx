4b010190a1f2b930e73090ed1470982e137d70e0
Matthew Incardona <doublethink.inc@gmail.com>
2025-12-29 16:38:38 -0500

    feat: change home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,122 @@
-import { Hero } from "home/Hero";
-import { Steps } from "home/Steps";
-import { Features } from "home/Features";
-import { Testimonials } from "home/Testimonials";
-import { QuestionsAndAnswers } from "home/QuestionsAndAnswers";
+"use client";
+import { useState, useEffect } from "react";
+import { readPdf } from "lib/parse-resume-from-pdf/read-pdf";
+import type { TextItems } from "lib/parse-resume-from-pdf/types";
+import { groupTextItemsIntoLines } from "lib/parse-resume-from-pdf/group-text-items-into-lines";
+import { groupLinesIntoSections } from "lib/parse-resume-from-pdf/group-lines-into-sections";
+import { extractResumeFromSections } from "lib/parse-resume-from-pdf/extract-resume-from-sections";
+import { ResumeDropzone } from "components/ResumeDropzone";
+import { cx } from "lib/cx";
+import { Heading, Link, Paragraph } from "components/documentation";
+import { ResumeTable } from "resume-parser/ResumeTable";
+import { FlexboxSpacer } from "components/FlexboxSpacer";
 
-export default function Home() {
+const RESUME_EXAMPLES = [
+  {
+    fileUrl: "resume-example/laverne-resume.pdf",
+    description: (
+      <span>
+        Borrowed from University of La Verne Career Center -{" "}
+        <Link href="https://laverne.edu/careers/wp-content/uploads/sites/15/2010/12/Undergraduate-Student-Resume-Examples.pdf">
+          Link
+        </Link>
+      </span>
+    ),
+  },
+  {
+    fileUrl: "resume-example/openresume-resume.pdf",
+    description: (
+      <span>
+        Created with OpenResume resume builder -{" "}
+        <Link href="/resume-builder">Link</Link>
+      </span>
+    ),
+  },
+];
+
+const defaultFileUrl = RESUME_EXAMPLES[0]["fileUrl"];
+export default function ResumeParser() {
+  const [fileUrl, setFileUrl] = useState(defaultFileUrl);
+  const [textItems, setTextItems] = useState<TextItems>([]);
+  const lines = groupTextItemsIntoLines(textItems || []);
+  const sections = groupLinesIntoSections(lines);
+  const resume = extractResumeFromSections(sections);
+
+  useEffect(() => {
+    async function test() {
+      const textItems = await readPdf(fileUrl);
+      setTextItems(textItems);
+    }
+    test();
+  }, [fileUrl]);
+
   return (
-    <main className="mx-auto max-w-screen-2xl bg-dot px-8 pb-32 text-gray-900 lg:px-12">
-      <Hero />
-      <Steps />
-      <Features />
-      <Testimonials />
-      <QuestionsAndAnswers />
+    <main className="h-full w-full overflow-hidden">
+      <div className="grid md:grid-cols-6">
+        <div className="flex justify-center px-2 md:col-span-3 md:h-[calc(100vh-var(--top-nav-bar-height))] md:justify-end">
+          <section className="mt-5 grow px-4 md:max-w-[600px] md:px-0">
+            <div className="aspect-h-[9.5] aspect-w-7">
+              <iframe src={`${fileUrl}#navpanes=0`} className="h-full w-full" />
+            </div>
+          </section>
+          <FlexboxSpacer maxWidth={45} className="hidden md:block" />
+        </div>
+        <div className="flex px-6 text-gray-900 md:col-span-3 md:h-[calc(100vh-var(--top-nav-bar-height))] md:overflow-y-scroll">
+          <FlexboxSpacer maxWidth={45} className="hidden md:block" />
+          <section className="max-w-[600px] grow">
+            <Heading className="text-primary !mt-4">Resume Parser</Heading>
+            <Paragraph smallMarginTop={true}>
+              Quickly check if your resume is ATS-ready. Powered by{" "}
+              <a
+                href="https://github.com/xitanggg/open-resume"
+                className="underline"
+                target="_blank"
+              >
+                Open Resume
+              </a>
+            </Paragraph>
+            <div className="mt-3 flex gap-3">
+              {RESUME_EXAMPLES.map((example, idx) => (
+                <article
+                  key={idx}
+                  className={cx(
+                    "flex-1 cursor-pointer rounded-md border-2 px-4 py-3 shadow-sm outline-none hover:bg-gray-50 focus:bg-gray-50",
+                    example.fileUrl === fileUrl
+                      ? "border-blue-400"
+                      : "border-gray-300"
+                  )}
+                  onClick={() => setFileUrl(example.fileUrl)}
+                  onKeyDown={(e) => {
+                    if (["Enter", " "].includes(e.key))
+                      setFileUrl(example.fileUrl);
+                  }}
+                  tabIndex={0}
+                >
+                  <h1 className="font-semibold">Resume Example {idx + 1}</h1>
+                  <p className="mt-2 text-sm text-gray-500">
+                    {example.description}
+                  </p>
+                </article>
+              ))}
+            </div>
+
+            <div className="mt-3">
+              <ResumeDropzone
+                onFileUrlChange={(fileUrl) =>
+                  setFileUrl(fileUrl || defaultFileUrl)
+                }
+                playgroundView={true}
+              />
+            </div>
+            <Heading level={2} className="!mt-[1.2em]">
+              Resume Parsing Results
+            </Heading>
+            <ResumeTable resume={resume} />
+
+            <div className="pt-24" />
+          </section>
+        </div>
+      </div>
     </main>
   );
 }
